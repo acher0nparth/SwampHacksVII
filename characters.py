ab8b1e1fc016f236769b1a951997af12b1d8bc6c
James Nguyen <jamesqnguyen1@ufl.edu>
2021-01-30 00:58:56 -0500

    Added jumping functionality
    : )

--- characters.py
+++ characters.py
@@ -3,8 +3,8 @@ import pygame as pg
 from pygame.locals import *
 pg.init()
 
-screen_width = 1920
-screen_height = 1080
+screen_width = 1280
+screen_height = 720
 
 screen = pg.display.set_mode([screen_width, screen_height])
 
@@ -13,44 +13,46 @@ player_walk = pg.image.load('gator_sprite2.png').convert_alpha()
 bulldog1 = pg.image.load('bulldog1.png').convert_alpha()
 
 class Gator(pg.sprite.Sprite):
+    left = False
+    right = False
+    isJump = False
+    jumpCount = 10
 
+    x = screen_width / 2
+    y = screen_height / 2
+    walkCount = 0
+    width = 64 #CHANGE BASED ON SIZE OF SPRITE
+    height = 64 #CHANGE BASED ON SIZE OF SPRITE
+    vel = 5
+    
     def __init__(self):
         super(Gator, self).__init__()
         self.surf = player_stand
         self.rect = self.surf.get_rect(
             center=(
-                random.randint(screen_height + 20, screen_width + 100),
-                random.randint(0, screen_height),
+                Gator.x, Gator.y
             )
          )
 
     def update(self, pressed_keys):
-        if pressed_keys[K_w] or pressed_keys[K_UP]:
-            self.surf = player_walk
-            self.rect.move_ip(0, -5)
-            self.surf = player_stand
-        if pressed_keys[K_DOWN] or pressed_keys[K_s]:
-            self.surf = player_walk
-            self.rect.move_ip(0, 5)
-            self.surf = player_stand
-        if pressed_keys[K_LEFT] or pressed_keys[K_a]:
-            self.surf = player_walk
-            self.rect.move_ip(-5, 0)
-            self.surf = player_stand
-        if pressed_keys[K_RIGHT] or pressed_keys[K_d]:
-            self.surf = player_walk
-            self.rect.move_ip(5, 0)
-            self.surf = player_stand
+        if (pressed_keys[K_LEFT] or pressed_keys[K_a]) and Gator.x > Gator.vel :
+            Gator.x -= Gator.vel
+        if (pressed_keys[K_RIGHT] or pressed_keys[K_d]) and Gator.x < screen_width - Gator.width - Gator.vel :
+            Gator.x += Gator.vel
 
-        if self.rect.left < 0:
-            self.rect.left = 0
-        if self.rect.right > screen_width:
-            self.rect.right = screen_width
-        if self.rect.top <= 0 :
-            self.rect.top = 0
-        if self.rect.bottom >= screen_height:
-            self.rect.bottom = screen_height
-
+        if not (Gator.isJump) :
+            if pressed_keys[K_SPACE] :
+                Gator.isJump = True
+        else :
+            if Gator.jumpCount >= -10 :
+                neg = 1
+                if Gator.jumpCount < 0 :
+                    neg = -1
+                Gator.y -= (Gator.jumpCount ** 2) * 0.5 * neg
+                Gator.jumpCount -= 1
+            else :
+                Gator.isJump = False
+                Gator.jumpCount = 10
 
 class enemy(pg.sprite.Sprite):
 
--- main.py
+++ main.py
@@ -5,77 +5,36 @@ from pygame.locals import *
 
 pg.init()
 
-<<<<<<< HEAD
-screen_width = 800
-screen_height = 600
-
+screen_width = 1280
+screen_height = 720
 screen = pg.display.set_mode([screen_width, screen_height])
 
-player_stand = pg.image.load('gator_sprite1.png').convert_alpha()
-player_walk = pg.image.load('gator_sprite2.png').convert_alpha()
+walkLeft = [pg.image.load('gator_sprite1.png').convert_alpha(), pg.image.load('gator_sprite2.png').convert_alpha(), pg.image.load('gator_sprite1.png').convert_alpha(), pg.image.load('gator_sprite2.png').convert_alpha(), pg.image.load('gator_sprite1.png').convert_alpha(), pg.image.load('gator_sprite2.png').convert_alpha()]
+walkRight = [pg.image.load('gator_sprite1.png').convert_alpha(), pg.image.load('gator_sprite2.png').convert_alpha(), pg.image.load('gator_sprite1.png').convert_alpha(), pg.image.load('gator_sprite2.png').convert_alpha(), pg.image.load('gator_sprite1.png').convert_alpha(), pg.image.load('gator_sprite2.png').convert_alpha()]
+
+background = pg.image.load('background.png')
 
 clock = pg.time.Clock()
-
-# class Enemy(pg.sprite.Sprite)
-
-class Gator(pg.sprite.Sprite):
-    def __init__(self):
-        super(Gator, self).__init__()
-        self.surf = player_stand
-        self.rect = self.surf.get_rect(
-            center=(
-                random.randint(screen_height + 20, screen_width + 100),
-                random.randint(0, screen_height),
-            )
-         )
-        self.speed = random.randint(5, 20)
-
-    def update(self):
-        if pressed_keys[K_w] or pressed_keys[K_UP]:
-            self.surf = player_walk
-            self.rect.move_ip(0, -5)
-            self.surf = player_stand
-        if pressed_keys[K_DOWN] or pressed_keys[K_s]:
-            self.surf = player_walk
-            self.rect.move_ip(0, 5)
-            self.surf = player_stand
-        if pressed_keys[K_LEFT] or pressed_keys[K_a]:
-            self.surf = player_walk
-            self.rect.move_ip(-5, 0)
-            self.surf = player_stand
-        if pressed_keys[K_RIGHT] or pressed_keys[K_d]:
-            self.surf = player_walk
-            self.rect.move_ip(5, 0)
-            self.surf = player_stand
-
-        if self.rect.left < 0:
-            self.rect.left = 0
-        if self.rect.right > screen_width:
-            self.rect.right = screen_width
-        if self.rect.top <= 0 :
-            self.rect.top = 0
-        if self.rect.bottom >= screen_height:
-            self.rect.bottom = screen_height
-
-player = Gator()
-=======
-screen_width = 1920
-screen_height = 1080
-
-screen = pg.display.set_mode([screen_width, screen_height])
-
-
-clock = pg.time.Clock()
-
-# class Enemy(pg.sprite.Sprite)
-
 player = characters.Gator()
 bulldog = characters.enemy()
->>>>>>> ethan
-# Run until the user asks to quit
+
+def redrawGameWindow() :
+    screen.blit(background, (0,0))
+    if player.walkCount + 1 >= 59 :
+        player.walkCount = 0
+    if player.left :
+        screen.blit(walkLeft[player.walkCount//6], (player.x,player.y))
+        player.walkCount += 1
+    
+    screen.blit(player.surf, player.rect)
+    screen.blit(bulldog.surf, bulldog.rect)
+    pg.display.update()
+
+
+
+#main
 running = True
 while running:
-
     #get every event in the queue
     for event in pg.event.get():
         if event.type == KEYDOWN :
@@ -87,26 +46,11 @@ while running:
             running = False
     
     pressed_keys = pg.key.get_pressed()
-<<<<<<< HEAD
-    player.update()
-    
-    # Fill the background with white
-    screen.fill((255, 255, 255))
-
-    screen.blit(player.surf, player.rect)
-
-=======
     player.update(pressed_keys)
-    
-    # Fill the background with white
-    screen.fill((2, 255, 25))
-
-    screen.blit(player.surf, player.rect)
-    screen.blit(bulldog.surf, bulldog.rect)
->>>>>>> ethan
-    # Flip the display
+    screen.fill((255, 255, 255))
+    pg.draw.rect(screen, (255, 0, 0), (player.x, player.y, player.width, player.height))
     pg.display.flip()
-
+    # redrawGameWindow()
     clock.tick(60)
 
 # Done! Time to quit.
